59d59943dcec7cc34d281264c2ba70c873cad8b2
MehdiKababy <mehdikababy@gmail.com>
2025-05-15 17:06:50 +0330

    update guess number

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-print("Welcome to Guess the number")
 import random
-def Number(orginal_Number, guess_Number):
-    def check_Number(org_Number, guess):
-        if guess == org_Number:
-            print("You Win")
-            return True
-        elif guess < org_Number:
-            print("Your guess is smaller")
-        elif guess > org_Number:
-            print("Your guess is bigger")
-    if 0 <= guess_Number <= 100:
-        result = check_Number(orginal_Number, guess_Number)
-    else:
-        print("The guess must be between 1 and 100")
-        return False
-    return result
-Run = 10
-number = random.randrange(0, 100)
-while Run > 0:
-    print(f'You have {Run} chances to guess')
-    input_Number = int(input("Guess what number it is (0 - 100) : "))
-    result = Number(number, input_Number)
-    if result == True:
-        break
-    elif result == False:
-        continue
-    Run -= 1
+print("Welcome to guess number")
+def guess_number():
+    Run = 10
+    number = random.randrange(0, 100)
+    while Run > 0:
+        try:            
+            print(f'\nYou have {Run} tries')
+            guess = int(input("Enter number : "))
+            if 0 < guess < 100:
+                if guess == number:
+                    print("\nCorrect. You Win")
+                    break
+                elif guess < number:
+                    print("Your guess is small")
+                elif guess > number:
+                    print("Your guess is bigger")
+                Run -= 1    
+            else:
+                print("The number must be 1 to 100")
+        except:
+            print("The input must be a number\nTry again")
+    if Run == 0 and guess != number:
+        print("\nYou lost")
+guess_number()
